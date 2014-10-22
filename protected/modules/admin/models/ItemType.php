21f42ccf825794527255d90581d2d1cb5102c7e8
max100491 <max100491@mail.ru>
2014-10-22 23:37:50 +0600

    admin model

--- protected/modules/admin/models/ItemType.php
+++ protected/modules/admin/models/ItemType.php
@@ -12,6 +12,17 @@
 class ItemType extends CActiveRecord
 {
 	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return ItemType the static model class
+	 */
+	
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+	/**
 	 * @return string the associated database table name
 	 */
 	public function tableName()
@@ -86,16 +97,5 @@ class ItemType extends CActiveRecord
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
 		));
-	}
-
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return ItemType the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
 	}
 }
---
+++ protected/modules/admin/models/Menu.php
@@ -0,0 +1,107 @@
+<?php
+class Menu extends CActiveRecord
+{
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	public function tableName()
+	{
+		return 'cf_menu';
+	}
+
+    public function behaviors(){
+        return array( 'EAdvancedArBehavior' => array(
+            'class' => 'application.behaviors.EAdvancedArBehavior'));
+    }
+
+	public function rules()
+	{
+		return array(
+			array('name_menu', 'length', 'max'=>255),
+			array('id_menu, name_menu', 'safe', 'on'=>'search'),
+		);
+	}
+
+	public function relations()
+	{
+		return array(
+            'items' => array(self::MANY_MANY, 'Items', 'cf_menu_items(menu_id, item_id)', 'condition'=>'status_id = 2'),
+		);
+	}
+
+	public function attributeLabels()
+	{
+		return array(
+			'id_menu' => '#',
+			'name_menu' => 'Наименование',
+		);
+	}
+
+	public function search()
+	{
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id_menu',$this->id_menu);
+		$criteria->compare('name_menu',$this->name_menu,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+    public function getList()
+    {
+        $model = self::model()->findAll();
+        return CHtml::listData($model, 'id_menu', 'name_menu');
+    }
+
+    /**
+     * Получить пункты меню по id меню. 
+     */
+    public function getItemMenuId($id, $order = 'sort_item ASC')
+    {
+        $model = self::model()->findByPk($id);
+        $items = array();
+        foreach($model->items(array('order'=>$order)) as $item){
+            switch ($item->type->transmit_id) {
+                case 0: $url = array($item->type->action_type);
+                    break;
+                case 1: $url = (!empty($item->pages) || $item->type->action_type!='/pages/index') ? array($item->type->action_type, 'id'=>$item->id_item) : '#';
+                    break;
+                case 2: $url = Yii::app()->homeUrl;
+                    break;
+            }
+            $items[] = array(
+                'label'=>$item->name_item,
+                'url'=>$url,
+                'items'=>$this->getItemNested($item->id_item)
+            );
+        }
+        return $items;
+    }
+
+    public function getItemNested($id, $order = 'sort_item ASC')
+    {
+        $model = Items::model()->findAllByAttributes(array('parent_id'=>$id, 'status_id'=>2));
+        if($model){
+            $items = array();
+            foreach($model as $item){
+                switch ($item->type->transmit_id) {
+                    case 0: $url = array($item->type->action_type);
+                        break;
+                    case 1: $url = array($item->type->action_type, 'id'=>$item->id_item);
+                        break;
+                    case 2: $url = Yii::app()->homeUrl;
+                        break;
+                }
+                $items[] = array(
+                    'label'=>$item->name_item,
+                    'url'=>$url,
+                );
+            }
+            return $items;
+        }
+    }
+}
